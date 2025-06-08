09ff234e10f524622abbfce0c58a8c06e9055077
Alexandre Clément <web@alexandreclement.com>
2025-06-07 22:02:03 -0700

    Update organizers list

--- src/config/organizers.ts
+++ src/config/organizers.ts
@@ -1,16 +1,11 @@
 export const ORGANIZERS: Record<string, { label: string, color: string, textColor?: 'black' | 'white' }> = {
-  'Thrashers': {
-    label: 'Thrashers',
-    color: '#ffed29',
-    textColor: 'black',
+  'Concord': {
+    label: 'Concord',
+    color: '#005daa',
   },
   'CRC': {
     label: 'Coastal Race Club',
     color: '#df2334',
-  },
-  'LocalRide': {
-    label: 'Local Ride',
-    color: '#3886c7'
   },
   'EscapeVelocity': {
     label: 'Escape Velocity',
@@ -20,24 +15,33 @@ export const ORGANIZERS: Record<string, { label: string, color: string, textColo
     label: 'GoodRide Gravel',
     color: '#e89d87',
   },
-  'ShimsRide': {
-    label: 'Shim\'s Ride',
-    color: '#de0112',
-  },
-  'Concord': {
-    label: 'Concord',
-    color: '#005daa',
-  },
-  'WestCoastCycling': {
-    label: 'West Coast Cycling',
-    color: '#2cace3',
+  'LocalRide': {
+    label: 'Local Ride',
+    color: '#3886c7'
   },
   'RideForWater': {
     label: 'Ride For Water',
     color: '#02b9d0',
   },
+  'Thrashers': {
+    label: 'Thrashers',
+    color: '#ffed29',
+    textColor: 'black',
+  },
+  'ShimsRide': {
+    label: 'Shim\'s Ride',
+    color: '#de0112',
+  },
+  'UnitedVelo': {
+    label: 'United Velo',
+    color: '#3aae49',
+  },
   'VictoriaCycling': {
     label: 'Victoria Cycling League',
     color: 'black',
-  }
+  },
+  'WestCoastCycling': {
+    label: 'West Coast Cycling',
+    color: '#2cace3',
+  },
 }
