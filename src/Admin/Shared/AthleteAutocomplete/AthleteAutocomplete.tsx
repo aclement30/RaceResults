da761c1df6210bb0ad9d85a8eb9221c3fe07ed0c
Alexandre Clément <web@alexandreclement.com>
2026-04-20 21:27:09 -0700

    Update AthleteAutocomplete field

--- src/Admin/Shared/AthleteAutocomplete/AthleteAutocomplete.tsx
+++ src/Admin/Shared/AthleteAutocomplete/AthleteAutocomplete.tsx
@@ -1,30 +1,65 @@
 import { Autocomplete, type AutocompleteProps } from '@mantine/core'
-import React, { useContext } from 'react'
+import { IconUserCheck } from '@tabler/icons-react'
+import React, { useCallback, useContext, useEffect, useState } from 'react'
 import { AdminContext } from '../AdminContext'
 
 type AthleteSelectProps = AutocompleteProps & {
   value: string
+  uciId?: string
   onChange: (value: string) => void
   onOptionSubmit?: (value: string) => void
 }
 
 export const AthleteAutocomplete = React.memo<AthleteSelectProps>(({
   value,
+  uciId,
   onChange,
   onOptionSubmit,
   ...restOfProps
 }) => {
-  const { athleteOptions } = useContext(AdminContext)
+  const { athleteOptions, findAthlete } = useContext(AdminContext)
+  const [isAthleteMatched, setIsAthleteMatched] = useState(false)
+
+  let rightSection = restOfProps.rightSection
+
+  if (!rightSection && isAthleteMatched) {
+    rightSection = <IconUserCheck size={14} color="var(--mantine-color-green-6)"/>
+  }
+
+  const handleOptionSubmit = useCallback((uciId: string) => {
+    setIsAthleteMatched(true)
+
+    onOptionSubmit?.(uciId)
+  }, [onOptionSubmit])
+
+  useEffect(() => {
+    const parts = value.trim().split(/\s+/)
+    const firstName = parts[0] || ''
+    const lastName = parts.slice(1).join(' ') || ''
+
+    const matchingAthlete = findAthlete({
+      uciId,
+      firstName,
+      lastName,
+    })
+
+    // Check if matching athlete's name matches the input value
+    // Sometimes the UCI ID passed from props may not match the name in the input, so we want to ensure the name also matches before showing the checkmark
+    const isNameMatching = `${matchingAthlete?.firstName} ${matchingAthlete?.lastName}`.toLowerCase().trim() === value.toLowerCase().trim()
+
+    setIsAthleteMatched(isNameMatching)
+  }, [value, uciId, athleteOptions])
 
   return (
     <Autocomplete
       value={value}
       data={athleteOptions}
       onChange={onChange}
-      onOptionSubmit={onOptionSubmit}
+      onOptionSubmit={handleOptionSubmit}
       placeholder="Name or search..."
       limit={10}
       {...restOfProps}
+      rightSection={rightSection}
     />
   )
 })
--- src/Admin/utils/loadAdminStartupData.ts
+++ src/Admin/utils/loadAdminStartupData.ts
@@ -2,15 +2,17 @@ import { fetchEventYears } from '../../utils/aws-s3'
 import { adminApi } from './api'
 
 export async function loadAdminStartupData() {
-  const [organizers, years, adminUsers] = await Promise.all([
+  const [organizers, years, adminUsers, athletesOverrides] = await Promise.all([
     adminApi.get.organizers(),
     fetchEventYears(),
-    adminApi.get.adminUsers()
+    adminApi.get.adminUsers(),
+    adminApi.get.athletesOverrides(),
   ])
 
   return {
     organizers,
     years,
     adminUsers,
+    athletesOverrides,
   }
 }
